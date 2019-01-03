d323f63c0f7c547edaf601671564803b2f92df2d
zaiyangliu <lzyxn@126.com>
2019-01-03 21:27:27 +0800

    a

---
+++ 21.MergeTwoSortedLists/1.cpp
@@ -0,0 +1,34 @@
+class Solution{
+public:
+  ListNode *mergeTwoLists(ListNode *l1, ListNode *l2){
+    ListNode *head = new ListNode(0);
+    ListNode *ptr = head;
+    while(l1 != NULL || l2 != NULL){
+      ListNode *current = NULL;
+      if(l1 == NULL){
+	current = l2;
+	l2 = l2 -> next;
+      }
+      else if(l2 == NULL){
+	current = l1;
+	l1 = l1 -> next;
+      }
+      else if(l1 == NULL && l2 == NULL){
+	return NULL;
+      }
+      else{
+	if(l1 -> val < l2 -> val){
+	  current = l1;
+	  l1 = l1 -> next;
+	}
+	else{
+	  current = l2;
+	  l2 = l2 -> next;
+	}
+      }
+      ptr -> next = current;
+      ptr = ptr -> next;
+    }
+    return head -> next;
+  }
+};
