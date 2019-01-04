c5340285fabc28feec126f2431db8f9466995276
zaiyangliul <lzyxn@126.com>
2019-01-04 14:25:19 +0800

    a

---
+++ 27.RemoveElement/1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+       if(nums.empty())
+           return 0;
+        int len = nums.size();
+        int j = 0;
+        for(int i = 0; i < len; ++i){
+            if(nums[i] != val)
+                nums[j++] = nums[i];
+        }
+        return j ;
+        
+    }
+};
